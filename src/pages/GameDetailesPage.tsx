4d81ef3951ec5d34ec0d6cc6a25c234ef4175c39
alire1992 <alirezasa24@gmail.com>
2024-03-22 23:42:21 +0330

    Refactor: redesign game details page

--- src/pages/GameDetailesPage.tsx
+++ src/pages/GameDetailesPage.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useGame } from "../hooks/useGame";
 
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
@@ -16,12 +16,21 @@ export default function GameDetailesPage() {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText limit={300}>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenShots gameId={game.id} />
-    </>
+    <SimpleGrid
+      columns={{
+        base: 1,
+        md: 2,
+      }}
+    >
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText limit={300}>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameScreenShots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 }
